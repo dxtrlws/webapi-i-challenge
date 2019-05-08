76dc945c62641d2a58d8a8a10dd3bf49f0e33d47
Dexter Lewis <dexter.lewis@gmail.com>
2019-05-08 17:57:51 -0400

    add routes for post, get and get by id

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+server.use(express.json());
+const db = require('./data/db');
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(({ code, message }) => {
+      res.status(code).json({ err: message });
+    });
+});
+
+server.get('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(({ code, message }) => {
+      res.status(code).json({ err: message });
+    });
+});
+
+server.post('/api/users', (req, res) => {
+  const newUser = req.body;
+  db.insert(newUser)
+    .then(addUser => {
+      res.status(201).send(addUser);
+    })
+    .catch(({ code, message }) => {
+      res.status(code).json({ err: message });
+    });
+});
+
+server.listen(9090, () => {
+  console.log('server running on port 9090');
+});
